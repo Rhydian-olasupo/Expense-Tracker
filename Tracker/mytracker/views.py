ec9a0293fdf9807862d486e6fd7fa12f614b6824
unknown <alfredolasupo@gmail.com>
2023-06-08 21:38:05 +0000

    api created

--- Tracker/mytracker/views.py
+++ Tracker/mytracker/views.py
@@ -7,20 +7,39 @@ from .models import Books,Category
 from django.http import Http404
 
 
-class CategoriesView(generics.ListCreateAPIView,generics.RetrieveUpdateDestroyAPIView):
+class CategoriesView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-class BookView(generics.ListCreateAPIView,generics.RetrieveUpdateDestroyAPIView):
-    queryset = Books.objects.all
-    serializer_class = BookSerializer
-
-class SingleBookView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = BookSerializer
+class CategorySingleView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'category'
 
     def get_object(self):
         try:
-            return Books.objects.get(id=self.kwargs['name'])
+            category = self.kwargs['category']
+            obj = self.get_queryset().get(category=category)
+            self.check_object_permissions(self.request, obj)
+            return obj
+        except Category.DoesNotExist:
+            raise Http404("Catgeory does not exist")
+
+class BookView(generics.ListCreateAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+
+class SingleBookView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'title'
+
+    def get_object(self):
+        try:
+            title = self.kwargs['title']
+            obj = self.get_queryset().get(title=title)
+            self.check_object_permissions(self.request, obj)
+            return obj
         except Books.DoesNotExist:
             raise Http404("Book not found")
 
