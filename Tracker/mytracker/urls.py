775d33e1e6f9642ec3e600e619017c5c1527885c
unknown <alfredolasupo@gmail.com>
2023-06-09 21:52:05 +0000

    booklist view

--- Tracker/mytracker/urls.py
+++ Tracker/mytracker/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('category', views.CategoriesView.as_view()),
     path('category/<str:category>',views.CategorySingleView.as_view()),
-    path('books',views.BookView.as_view()),
-    path('books/<str:title>',views.SingleBookView.as_view(),)
+    path('booklist',views.BookView.as_view()),
+    path('books/<str:title>',views.SingleBookView.as_view()),
+    path('book/', views.books, name="book"),
+    path('books/',views.book_list,name='book-list')
 ]
--- Tracker/mytracker/views.py
+++ Tracker/mytracker/views.py
@@ -5,6 +5,8 @@ from rest_framework import generics
 from .serializers import CategorySerializer,BookSerializer
 from .models import Books,Category
 from django.http import Http404
+from .forms import BookingForm
+import requests
 
 
 class CategoriesView(generics.ListCreateAPIView):
@@ -43,3 +45,49 @@ class SingleBookView(generics.RetrieveUpdateDestroyAPIView):
         except Books.DoesNotExist:
             raise Http404("Book not found")
 
+
+def books(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request,'books.html',context)
+
+
+'''def book_list(request):
+    response = requests.get('http://127.0.0.1:8000/api/booklist')
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return render(request,'booklist.html',{'Books':data})
+    else:
+        error_message = f'Error: {response.status_code}'
+        return render(request,'error.html',{'message':error_message})'''
+    
+def book_list(request):
+    url = 'http://127.0.0.1:8000/api/booklist'
+    books = []  # List to store all books
+
+    while url:
+        # Make a GET request to the backend API
+        response = requests.get(url)
+
+        # Check the response status code
+        if response.status_code == 200:
+            # API request was successful
+            data = response.json()  # Extract the JSON data from the response
+
+            books.extend(data['results'])  # Add books to the list
+
+            # Check if there is a next page
+            url = data['next']
+        else:
+            # API request encountered an error
+            error_message = f'Error: {response.status_code}'
+            return render(request, 'error.html', {'message': error_message})
+
+    # Pass the book data to the template for rendering
+    return render(request, 'booklist.html', {'books': books})
